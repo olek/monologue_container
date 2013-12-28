1499f329c27136c51838c6d00e8702b45e9a45cf
Olek Poplavsky <olek@woodenbits.com>
2013-12-28 14:54:47 -0500

    Create container rails app, configure monologue.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MonologueContainer::Application.config.secret_token = '6cddd40a89e568aecd00831456da54c483625825cdde32e6cd295a826ff3288f292121eb51cef63e17dafb1bfa7b6d8bf4987d6bc8e5dd2a8b403815604596ba'
