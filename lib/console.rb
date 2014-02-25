9eba622d48af10976a91d192ed4be562ab4f6535
Olek Poplavsky <olek@woodenbits.com>
2014-02-24 21:23:18 -0500

    adjust to use session instead of console hackery

--- lib/console.rb
+++ lib/console.rb
@@ -1,13 +1,6 @@
 M = Monologue
 
-# make more conveniently accessible
-module Repos
-  M::Repos.keys.each do |entity_class|
-    name = entity_class.name.underscore.split('/').second.to_sym
-    self.class.class_eval do
-      define_method(name) {
-        M::Repos[entity_class]
-      }
-    end
-  end
+# make conveniently accessible
+def create_session
+  ORMivore::Session.new(M::Repos)
 end
